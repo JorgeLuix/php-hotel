6068de9422a6f780b6cf605cf1594285048e84c3
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-05 00:15:21 +0200

    agginunti filter parking e vote

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 $hotels = [
     [
         'name' => 'Hotel Belvedere',
@@ -38,13 +37,80 @@ $hotels = [
     ],
 ];
 
-foreach ($hotels as $hotel) {
-    echo "Name: " . $hotel['name'] . "<br>";
-    echo "Description: " . $hotel['description'] . "<br>";
-    echo "Parking: " . ($hotel['parking'] ? "Yes" : "No") . "<br>";
-    echo "Vote: " . $hotel['vote'] . "<br>";
-    echo "Distance to center: " . $hotel['distance_to_center'] . "<br><br>";
-}
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>PHP Hotel</title>
+</head>
+<body>
+
+<div class="container">
+    <h1>Hotel List</h1>
+    <form class="form d-flexz" method="get">
+        <div class="form-group mb-3">
+            <label for="parking">Parking</label>
+            <select class="form-control" id="parking" name="parking">
+                <option value="">All</option>
+                <option value="1">Yes</option>
+                <option value="0">No</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="vote">Vote</label>
+            <select class="form-control" id="vote" name="vote">
+                <option value="">All</option>
+                <option value="1">1 star or higher</option>
+                <option value="2">2 stars or higher</option>
+                <option value="3">3 stars or higher</option>
+                <option value="4">4 stars or higher</option>
+                <option value="5">5 stars only</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-success my-3">Filter</button>
+    </form>
+
+    <br>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Vote</th>
+                <th>Distance to Center</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            $parkingFilter = isset($_GET['parking']) ? $_GET['parking'] : '';
+            $voteFilter = isset($_GET['vote']) ? $_GET['vote'] : '';
+
+            foreach ($hotels as $hotel) {
+
+                if (($parkingFilter == '' || $hotel['parking'] == ($parkingFilter == '1')) && ($voteFilter == '' || $hotel['vote'] >= $voteFilter)) {
+
+                    echo "<tr>";
+                    echo "<td>" . $hotel['name'] . "</td>";
+                    echo "<td>" . $hotel['description'] . "</td>";
+                    echo "<td>" . ($hotel['parking'] ? 'Yes' : 'No') . "</td>";
+                    echo "<td>" . $hotel['vote'] . "</td>";
+                    echo "<td>" . $hotel['distance_to_center'] . "</td>";
+                    echo "</tr>";
+                }
+            }
+        ?>
+        </tbody>
+    </table>
+</div>
+
+</body>
+</html>
 
